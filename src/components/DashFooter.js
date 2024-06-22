9ae23a597f132af2776d25db690f30989eead8a1
jcho0156 <jcho0156@student.monash.edu>
2024-06-22 14:40:10 +0800

    working react before redux

---
+++ src/components/DashFooter.js
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { faHouse } from "@fortawesome/free-solid-svg-icons"
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const DashFooter = () => {
+
+    const navigate = useNavigate();
+    const {pathname}= useLocation()
+
+    const onGoHomeClicked = () => {
+        navigate('/dash');
+    }
+
+    let goHomeButton = null;
+
+    if (pathname !== '/dash'){
+        goHomeButton = (
+            <button className="dash-footer__button icon-button" 
+            title="Home" onClick={onGoHomeClicked}>
+                <FontAwesomeIcon icon={faHouse} />
+            </button>
+        )
+    }
+  return (
+    <footer className="dash-footer">
+        {goHomeButton}
+        <p>Current user:</p>
+        <p>Status:</p>
+    </footer>
+  )
+}
+
+export default DashFooter
+ 
